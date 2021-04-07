44d4d928f627412820b3a400bd8539598d7e0c13
JuHyerin <jhr67370719@gmail.com>
2021-04-07 19:01:00 +0900

    TodoList (on progress)

--- src/App.js
+++ src/App.js
@@ -1,54 +1,24 @@
 import React from 'react';
-import './App.scss';
-import Button from './components/Button';
+import {createGlobalStyle} from 'styled-components';
+import TodoTemplate from "./components/Todo Template";
+import ToDoHead from "./components/Todo Head";
 
+
+const GlobalStyle = createGlobalStyle`
+        body {
+            background: #e9ecef;
+        }
+    `;
 function App() {
     return (
-        <div className="App">
-            <div className="buttons">
-                <Button size="large">BUTTON</Button>
-                <Button>BUTTON</Button>
-                <Button size="small">BUTTON</Button>
-            </div>
-            <div className="buttons">
-                <Button size="large" color="gray">
-                    BUTTON
-                </Button>
-                <Button color="gray">BUTTON</Button>
-                <Button size="small" color="gray">
-                    BUTTON
-                </Button>
-            </div>
-            <div className="buttons">
-                <Button size="large" color="pink">
-                    BUTTON
-                </Button>
-                <Button color="pink">BUTTON</Button>
-                <Button size="small" color="pink">
-                    BUTTON
-                </Button>
-            </div>
-            <div className="buttons">
-                <Button size="large" color="blue" outline>
-                    BUTTON
-                </Button>
-                <Button color="gray" outline>
-                    BUTTON
-                </Button>
-                <Button size="small" color="pink" outline>
-                    BUTTON
-                </Button>
-            </div>
-            <div className="buttons">
-                <Button  color="blue" fullwidth>
-                    BUTTON
-                </Button>
-                <Button color="pink" fullwidth>
-                    BUTTON
-                </Button>
-            </div>
-        </div>
+        <>
+            <GlobalStyle/>
+            <TodoTemplate>
+                <ToDoHead/>
+            </TodoTemplate>
+        </>
     );
+
 }
 
 export default App;
